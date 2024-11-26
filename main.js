5e1b399ff1b711d198fbabe8db17e50ab440adc9
Entes-steinla <149874891+Entes-steinla@users.noreply.github.com>
2024-11-26 15:19:30 +0700

    background sound

---
+++ main.js
@@ -0,0 +1,19 @@
+// window.addEventListener("load", () => {
+//   const autoPlaySound = new Audio("./background_sound/Pasilyo.mp3");
+//   autoPlaySound.loop = true;
+//   autoPlaySound.play();
+// });
+
+window.addEventListener("load", () => {
+  const autoPlaySound = new Audio("./background_sound/Pasilyo.mp3");
+
+  autoPlaySound.loop = true;
+
+  autoPlaySound.play();
+
+  localStorage.setItem("userInteracted", "true");
+
+  if (localStorage.getItem("userInteracted")) {
+    autoPlaySound.play();
+  }
+});
